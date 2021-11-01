8beda055e5586f0c23c8dbf46410b790ffb5438d
Marcelo Arteaga <arteagamarcelo@gmail.com>
2021-11-01 14:46:34 -0400

    update links for search

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
       <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
         <div class="navbar-start">
           <div class="navbar-item">
-            <form method="get" action="/search">
+            <form method="get" action="./search">
               <div class="field has-addons">
                 <div class="control">
                   <input type="text" class="input" placeholder="What are you looking for?" name="query">
